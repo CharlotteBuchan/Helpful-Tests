3b61f4bc390c273fe03cfcb7681d55c72f3b526d
ESCG\530240 <530240@chrome.escg.ac.uk>
2022-10-10 16:49:42 +0100

    im so confused

--- Input, Output, Integers/Input, Output, Integers/Program.cs
+++ Input, Output, Integers/Input, Output, Integers/Program.cs
@@ -4,11 +4,31 @@ namespace MyApp
 {
     internal class Program
     {
+        static int MonsterHealth = 100;
         static void Main(string[] args)
         {
-            int MonsterHealth;
-            
-            Console.WriteLine("Oh no! A monster has appeared! Quick, attack it!! (Q, E, X, C)");
+            Console.Write("Oh no! A monster has appeared! Quick, attack it!! (Q, E, X, C, R) ");
+            string Input = Console.ReadLine();
+            Attacks(Input);
+        }
+
+        static void Attacks(AttackInput)
+        {
+            switch (AttackInput.ToUpper())
+            {
+                case "Q":
+                    Console.WriteLine
+                    MonsterHealth = MonsterHealth - 15;
+                    break;
+                case "E":
+                    break;
+                case "X":
+                    break;
+                case "C":
+                    break;
+                case "R":
+                    break;
+            }
         }
     }
 }
