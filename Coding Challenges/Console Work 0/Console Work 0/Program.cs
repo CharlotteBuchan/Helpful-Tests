889f50f02db23d0c1e6353925ef1fca156eb2c6e
ESCG\530240 <530240@chrome.escg.ac.uk>
2022-10-17 15:54:39 +0100

    changing 0.2 to switch statements

--- Coding Challenges/Console Work 0/Console Work 0/Program.cs
+++ Coding Challenges/Console Work 0/Console Work 0/Program.cs
@@ -10,70 +10,73 @@ namespace MyApp
 
             Console.Write("Oh no! There is a monster!! ATTACK IT!!! (Q, E, R, Z, X) ");
             char Attack = Convert.ToChar(Console.ReadLine());
-            if (Attack == 'Q')
+            switch (Attack)
             {
-                MonsterHealth = (MonsterHealth - 15);
-                if (MonsterHealth > 0)
-                {
-                    Console.WriteLine($"The monster is at {MonsterHealth} health!");
+                case 'Q':
+                    MonsterHealth = (MonsterHealth - 15);
+                    if (MonsterHealth > 0)
+                    {
+                        Console.WriteLine($"The monster is at {MonsterHealth} health!");
+                        Main(null);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You killed the monster! Lets goooooo!");
+                    }
+                    break;
+                case 'E':
+                    MonsterHealth = (MonsterHealth - 12);
+                    if (MonsterHealth > 0)
+                    {
+                        Console.WriteLine($"The monster is at {MonsterHealth} health!");
+                        Main(null);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You killed the monster! Lets goooooo!");
+                    }
+                    break;
+
+                case 'R':
+                    MonsterHealth = (MonsterHealth - 20);
+                    if (MonsterHealth > 0)
+                    {
+                        Console.WriteLine($"The monster is at {MonsterHealth} health!");
+                        Main(null);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You killed the monster! Lets goooooo!");
+                    }
+                    break;
+                case 'Z':
+                    MonsterHealth = (MonsterHealth - 25);
+                    if (MonsterHealth > 0)
+                    {
+                        Console.WriteLine($"The monster is at {MonsterHealth} health!");
+                        Main(null);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You killed the monster! Lets goooooo!");
+                    }
+                    break;
+                case 'X':
+                    MonsterHealth = (MonsterHealth - 35);
+                    if (MonsterHealth > 0)
+                    {
+                        Console.WriteLine($"The monster is at {MonsterHealth} health!");
+                        Main(null);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You killed the monster! Lets goooooo!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Oh no! That is an incorrect key!");
                     Main(null);
-                }
-                else
-                {
-                    Console.WriteLine("You killed the monster! Lets goooooo!");
-                }
-            }
-            else if (Attack == 'E')
-            {
-                MonsterHealth = (MonsterHealth - 12);
-                if (MonsterHealth > 0)
-                {
-                    Console.WriteLine($"The monster is at {MonsterHealth} health!");
-                    Main(null);
-                }
-                else
-                {
-                    Console.WriteLine("You killed the monster! Lets goooooo!");
-                }
-            }
-            else if (Attack == 'R')
-            {
-                MonsterHealth = (MonsterHealth - 20);
-                if (MonsterHealth > 0)
-                {
-                    Console.WriteLine($"The monster is at {MonsterHealth} health!");
-                    Main(null);
-                }
-                else
-                {
-                    Console.WriteLine("You killed the monster! Lets goooooo!");
-                }
-            }
-            else if (Attack == 'Z')
-            {
-                MonsterHealth = (MonsterHealth - 25);
-                if (MonsterHealth > 0)
-                {
-                    Console.WriteLine($"The monster is at {MonsterHealth} health!");
-                    Main(null);
-                }
-                else
-                {
-                    Console.WriteLine("You killed the monster! Lets goooooo!");
-                }
-            }
-            else if (Attack == 'X')
-            {
-                MonsterHealth = (MonsterHealth - 35);
-                if (MonsterHealth > 0)
-                {
-                    Console.WriteLine($"The monster is at {MonsterHealth} health!");
-                    Main(null);
-                }
-                else
-                {
-                    Console.WriteLine("You killed the monster! Lets goooooo!");
-                }
+                    break;
             }
         }
     }
