28c8e76415e111c64a4c9d0aadb929aa5b45e06e
ESCG\530240 <530240@chrome.escg.ac.uk>
2022-10-18 13:27:15 +0100

    Started Task 5 - Spheres

---
+++ Coding Challenges/Starter Task 5 - Spheres/Starter Task 5 - Spheres/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.1415926535897931;
+            Console.Write("Enter the radius of your sphere: ");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            double area = (radius * radius * PI * 4);
+            double volume = (radius * radius * radius * PI * 4/3);
+            Console.WriteLine($"The surface area of your sphere is {area}, and the volume is {volume}");
+        }
+    }
+}
