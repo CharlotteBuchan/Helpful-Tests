749df7c90f6950986322bc2574ab9576ac6188a2
ESCG\530240 <530240@chrome.escg.ac.uk>
2022-10-31 15:43:25 +0000

    2.2 Challenege

---
+++ Coding Challenges/Console Work 2/Console Work 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Invalids = 0;
+            List<string> Names = new List<string>()
+            {
+                "Biff", "Chip", "Kipper", "Hans", "Super"
+            };
+
+            foreach (string name in Names)
+            {
+                if (name.Length == 4)
+                {
+                    Console.WriteLine($"{name}");
+                }
+                else
+                {
+                    Console.WriteLine($"{name} is invalid");
+                    Invalids = Invalids + 1;
+                }
+            }
+            Console.WriteLine($"{Invalids} invalids.");
+        }
+    }
+}
