4f04c9b69c27ba0a745c19780f260b0d0fded5c1
ESCG\530240 <530240@chrome.escg.ac.uk>
2022-11-30 16:22:26 +0000

    Nigel's coding challenges stuff

---
+++ files/files/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.IO;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer;
+            writer = new StreamWriter("test.txt");
+
+            writer.WriteLine("hello world");
+            writer.Close();
+
+            Console.WriteLine("done");
+
+            var fileName = @"D:\charlotte.txt";
+            var fileName2 = @"D:\work\charlotte.txt";
+            string copy = "This is not a dummy text";
+
+            File.WriteAllText(fileName, "This is dummy text");
+            // Create the file, or overwrite if the file exists.
+            File.Move(fileName, fileName2);
+
+
+            Console.WriteLine(File.GetLastAccessTime(fileName));
+            
+
+            /*
+            using FileStream fs = File.Create(fileName);
+            using var sr = new StreamWriter(fs);
+
+            sr.WriteLine(copy);
+            */
+            Console.WriteLine("done");
+
+        
+
+
+        }
+    }
