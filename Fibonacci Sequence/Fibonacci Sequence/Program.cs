c54f6421ec67fd9e210f19b58164f736bd655c43
ESCG\530240 <530240@chrome.escg.ac.uk>
2022-12-06 13:33:13 +0000

    Fibonacci Sequence

---
+++ Fibonacci Sequence/Fibonacci Sequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Fibonacci(num1, num2);
+        }
+
+        static void Fibonacci(int num, int previousNum)
+        {
+            int holder;
+            int i = 0;
+            while (i <= 50)
+            {
+                Console.WriteLine(num);
+                holder = num;
+                num = num + previousNum;
+                previousNum = holder;
+                i += 1;
+            }
+        }
+    }
+}
