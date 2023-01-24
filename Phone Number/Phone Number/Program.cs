1ebf01adf034abbf01d78223547b31a15bc88d49
ESCG\530240 <530240@chrome.escg.ac.uk>
2023-01-24 14:41:36 +0000

    TILEMAPSSSSSSSSSSSSS

---
+++ Phone Number/Phone Number/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void PhoneNumber()
+        {
+            Console.WriteLine("Enter phone number");
+            string number = Console.ReadLine();
+            if (number[0] == 0)
+            {
+
+            }
+        }
+    }
+}
