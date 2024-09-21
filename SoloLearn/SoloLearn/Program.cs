0936d0d3c2451b8623a12de09c3b27ae577d78a1
CharlotteBuchan <530240@chrome.escg.ac.uk>
2024-09-21 21:00:24 +0100

    idk

---
+++ SoloLearn/SoloLearn/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 89;
+            Console.WriteLine("Wow {0}", x);
+        }
+    }
+}
