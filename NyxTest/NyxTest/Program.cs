672fa52e586da843d302cc4037375d6abcd23d2d
CharlotteBuchan <530240@chrome.escg.ac.uk>
2024-10-01 06:53:33 +0100

    e

---
+++ NyxTest/NyxTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int optionsAnswered = 0;
+            int optionPicked;
+
+            do
+            {
+                Console.WriteLine("All 3 options: 1. ... 2. ... 3. ...");
+                optionPicked = Convert.ToInt32(Console.ReadLine());
+                if (optionPicked == 1)
+                {
+                    Console.WriteLine("Answer to 1");
+                    optionsAnswered++;
+                }
+                else if (optionPicked == 2)
+                {
+                    Console.WriteLine("Answer to 2");
+                    optionsAnswered++;
+                }
+                else if (optionPicked == 3)
+                {
+                    Console.WriteLine("No answer");
+                    break;
+                }
+                else if (optionPicked > 3)
+                {
+                    Console.WriteLine("Incorrect Option");
+                }
+            }
+            while (optionPicked != 3);
+        }
+    }
+}
